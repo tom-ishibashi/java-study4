b435769093d799fa3701efee83e0a770a267c7d5
tom-ishibashi <biz.double.moon84@gmail.com>
2017-04-16 00:59:55 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # java-study4
+
+# Description
+Java学習用のアプリケーションです
+
+# Requirements
+JDK 8
+Maven 3.x
+
+# How to run
+```
+mvn exec:java
+```
---
+++ src/main/java/com/javastudy4/main/Application.java
@@ -0,0 +1,10 @@
+package com.javastudy4.main;
+
+/**
+ * Created by bassyMac on 2017/04/15.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello world !!");
+    }
+}
