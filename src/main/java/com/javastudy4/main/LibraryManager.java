d953bad74d9170ed27c659388c71a14a0e4e59d4
tom ishibashi <biz.double.moon84@gmail.com>
2017-04-22 00:48:58 +0000

    操作選択の文言を表示できるよう修正

---
+++ src/main/java/com/javastudy4/main/LibraryManager.java
@@ -0,0 +1,11 @@
+package com.javastudy4.main;
+
+/**
+ * @author tom-ishibashi
+ */
+public class LibraryManager {
+    public static void main(String[] args) {
+        System.out.println("操作を選択してください。");
+        System.out.println("1.select 2.insert 3.update 4.delete");
+    }
+}
