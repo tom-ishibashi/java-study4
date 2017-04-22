5cf61474a3f4d0d9081df605537b255969cf9f5c
tom ishibashi <biz.double.moon84@gmail.com>
2017-04-22 02:52:18 +0000

    図書館のダミーデータ作成

--- src/main/java/com/javastudy4/operation/SelectOperation.java
+++ src/main/java/com/javastudy4/operation/SelectOperation.java
@@ -1,5 +1,24 @@
 package com.javastudy4.operation;
 
+import com.javastudy4.model.Library;
+import com.javastudy4.model.Book;
+import java.util.List;
+import java.util.ArrayList;
+
 public class SelectOperation {
-    
+    public void search(){
+        
+    }
+    private Library createDummyData(){
+        Book book = new Book();
+        book.setId(100);
+        book.setTitle("サルでもデキるjava");
+        List<Book> list = new ArrayList<>();
+        list.add(book);
+        Library library = new Library();
+        library.setBooks(list);
+        library.setId(1000);
+        library.setName("赤羽図書館");
+        return library;
+    }
 }
