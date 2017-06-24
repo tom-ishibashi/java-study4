1f790cb8586a879f8ae1d183ee2f17ee9e673681
tom ishibashi <biz.double.moon84@gmail.com>
2017-06-24 01:39:07 +0000

    戻り値のsqlを作成

--- src/main/java/com/javastudy4/service/LibraryService.java
+++ src/main/java/com/javastudy4/service/LibraryService.java
@@ -3,8 +3,8 @@ package com.javastudy4.service;
 public class LibraryService {
     
     public List<Library> selectByConndition(String libraryName){
-        
-        
+        String sql = buildSelectByCondittion(libraryName);
+        return new ArrayList();
     }
     
 }
--- src/main/java/com/javastudy4/service/SqlBuilder.java
+++ src/main/java/com/javastudy4/service/SqlBuilder.java
@@ -3,7 +3,12 @@ package com.javastudy4.service;
 public class SqlBuilder {
  
     public String buildSelectByCondittion(String libraryName){
-        return "aa";
+        StringBuilder sql = new StringBuilder();
+        sql.append("select * from Library");
+        sql.append(" where name = " + libraryName);
+        return sql;
     }
+    
+    
     
 }
