5ac59515117286a5fc228183da715caea9ee1d29
tom ishibashi <biz.double.moon84@gmail.com>
2017-06-24 02:07:10 +0000

    Daoのコネクション取得を実装

--- src/main/java/com/javastudy4/dao/LibraryDao.java
+++ src/main/java/com/javastudy4/dao/LibraryDao.java
@@ -1,11 +1,30 @@
 package com.javastudy4.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class LibraryDao {
     
     private Connection con;
     
     private void getConnection() {
-        
+        try {
+            // MySQLに接続
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javastudy4", "root", "");
+            System.out.println("MySQLに接続できました。");
+        } catch (SQLException e) {
+            System.out.println("MySQLに接続できませんでした。");
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.out.println("MySQLのクローズに失敗しました。");
+                }
+            }
+        }
     }
     
 }
