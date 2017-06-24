a621de357551e997ecc3e36307458b358ac5e785
tom ishibashi <biz.double.moon84@gmail.com>
2017-06-24 02:39:06 +0000

    検索の実装

--- src/main/java/com/javastudy4/dao/LibraryDao.java
+++ src/main/java/com/javastudy4/dao/LibraryDao.java
@@ -1,13 +1,31 @@
 package com.javastudy4.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import com.javastudy4.model.Library;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
 
 public class LibraryDao {
     
     private Connection con;
+    
+    public List<Library> select(String sql) throws SQLException{
+        getConnection();
+        Statement stm = con.createStatement();
+        ResultSet rs = stm.executeQuery(sql);
+        return convert(rs);
+    }
+    
+    private List<Library> convert(ResultSet rs) throws SQLException{
+        List<Library> result = new ArrayList<>();
+        while(rs.next()){
+            Library library = new Library();
+            library.setId(rs.getInt("id"));
+            library.setName(rs.getString("name"));
+            result.add(library);
+        }
+        return result;
+    }
     
     private void getConnection() {
         try {
